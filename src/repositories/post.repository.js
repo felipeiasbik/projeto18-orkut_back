bc341a496ad99de93532b2465f64629b737ca977
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-05-24 22:19:01 -0300

    feat: user post, list of posts by user and timeline

---
+++ src/repositories/post.repository.js
@@ -0,0 +1,24 @@
+import { db } from "../database/database.connection.js";
+
+export function tokenExistsDB(token){
+    const result = db.query(`SELECT * FROM "blackListToken" WHERE token = $1;`, [token]);
+    return result;
+}
+
+export function postImageDB(userId, photo, description){
+    const result = db.query(`
+    INSERT INTO post ("userId", photo, description)
+    VALUES ($1, $2, $3)
+    ;`,[userId.idUser, photo, description]);
+    return result;
+}
+
+export function myPostsDB(userId){
+    const result = db.query(`SELECT * FROM post WHERE "userId" = $1;`, [userId.idUser]);
+    return result;
+}
+
+export function listPostsDB(){
+    const result = db.query(`SELECT * FROM post ORDER BY "createdAt" DESC;`);
+    return result;
+}
--- src/schemas/post.schema.js
+++ src/schemas/post.schema.js
@@ -2,5 +2,5 @@ import joi from "joi";
 
 export const postImageSchema = joi.object({
     photo: joi.string().min(3).required(),
-    description: joi.string().min(3).required()
+    description: joi.string()
 });
